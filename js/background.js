88a1eb11e07e372ccc7331eb69f1e77e6f4b7977
Luca Pesavento <l.pesa91@gmail.com>
2015-07-12 18:25:27 +0200

    no message

--- js/background.js
+++ js/background.js
@@ -5,8 +5,28 @@
 	var placebg = function(){
 
 		//get browser window size
-		var width = window.innerWidth || document.body.clientWidth;
-		var height = window.innerHeight || document.body.clientHeight;
+		var width;
+		var height;
+
+		// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+		if (typeof window.innerWidth != 'undefined') {
+		  width = window.innerWidth,
+		  height = window.innerHeight
+		}
+
+		// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+		else if (typeof document.documentElement != 'undefined'
+		&& typeof document.documentElement.clientWidth !=
+		'undefined' && document.documentElement.clientWidth != 0) {
+		   width = document.documentElement.clientWidth,
+		   height = document.documentElement.clientHeight
+		}
+
+		// older versions of IE
+		else {
+		  width = document.getElementsByTagName('body')[0].clientWidth,
+		  height = document.getElementsByTagName('body')[0].clientHeight
+		}
 		
 		var aspectRatio = 0.667192740185440915367922;
 
